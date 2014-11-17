16a071b1849d6414f670f2457728ec0337ff2031
Andreas Otten <ao@joinmedia.de>
2014-11-17 11:51:52 +0100

    completed v1.0

--- jquery.smoothwheel.js
+++ jquery.smoothwheel.js
@@ -1,90 +1,63 @@
 /*!
- * utils.smoothwheel
+ * jQuery Smoothwheel Plugin v1.0
  *
- * Created by aotten77 on 14.11.2014.
- *
+ * Copyright (c) 2014 Andreas Otten (aotten77)
  * Released under the MIT license
  */
-define(['jquery', '_super', 'rAF', 'jquery_mousewheel'], function ($, _super){
-	'use strict';
-	var smoothwheel = $.extend({}, _super, {
-		init: function (options, elem) {
-			this.options = $.extend({}, this.options, options);
-			this.name = this.options.pluginName;
-			this.pos = this.options.pos;
-			this.jmname = this.options.jmname;
-			this.$elem = $(elem);
-			_super.config.call(this);
+(function (factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+	$.smoothwheel = function(options){
+		if(window.smoothwheel) return;
+		window.smoothwheel = true;
+		var opt = $.extend({
+			friction: 0.2,
+			deltaSteps: 100
+		}, options);
 
-			this.onInitExec();
-			return this;
-		},
+		var raf,
+			scrollwheel = false,
+			$window = $(window),
+			$document = $(document),
+			viewHeight = $window.height(),
+			$body = (navigator.userAgent.indexOf('AppleWebKit') !== -1) ? $('body') : $('html'),
+			viewY = $body.scrollTop(),
+			scrollToX = $body.scrollLeft(),
+			scrollToY = $body.scrollTop();
 
-		onInitExec: function () {
-			// https://github.com/simov/simplr-smoothscroll/blob/master/lib/jquery.simplr.smoothscroll.js
-			var that = this;
-			this.onAnimationFrame = this.onAnimationFrame.bind(this);
-			this.viewHeight = $(window).height();
-			this.$window = $(window);
-			this.$body = (navigator.userAgent.indexOf('AppleWebKit') !== -1) ? $('body') : $('html');
-			this.scrollToX = this.$body.scrollLeft();
-			this.scrollToY = this.$body.scrollTop();
-			this.viewY = 0;
-			this.scrollwheel = false;
-			this.docHeight = this.getDocHeight();
-			this.$window.on('resize', function () {
-				that.viewHeight = that.$window.height();
-				that.docHeight = that.getDocHeight();
-			}).on('scroll', function () {
-				that.scrollToX = that.$body.scrollLeft();
-				if (!that.scrollwheel)	that.scrollToY = that.viewY = that.$window.scrollTop();
-				console.log(that.scrollToY);
-			});
-			this.$body.mousewheel(function(event){
-				//var deltaFactor = event.deltaFactor;
-				var deltaFactor = event.deltaFactor * 100/Math.abs(event.deltaFactor);
-				//console.log(event.deltaFactor, deltaFactor);
-				that.scrollwheel = true;
-				if(event.deltaY < 0){
-					that.scrollToY = (that.scrollToY + that.viewHeight) >= that.docHeight ? that.scrollToY : that.scrollToY += deltaFactor;
-				}else{
-					that.scrollToY = that.scrollToY <= 0 ? 0 : that.scrollToY -= deltaFactor;
-				}
-				that.raf = requestAnimationFrame(that.onAnimationFrame);
-				return false;
-			});
-
-
-		},
-
-
-		options: {
-
-		},
-
-		_exec: function(e){
-			this.raf = requestAnimationFrame(this.onAnimationFrame);
-		},
-
-		//http://james.padolsey.com/javascript/get-document-height-cross-browser/
-		getDocHeight: function(){
-			var d = document;
-			return Math.max(d.body.scrollHeight, d.documentElement.scrollHeight, d.body.offsetHeight, d.documentElement.offsetHeight, d.body.clientHeight, d.documentElement.clientHeight);
-		},
-
-		onAnimationFrame: function(){
-			console.log(this.scrollToY);
-			this.viewY += (this.scrollToY - this.viewY) * 0.2;
-			window.scrollTo(this.scrollToX, Math.round(this.viewY));
-			if(window.pageYOffset !== this.scrollToY){
-				this.raf = requestAnimationFrame(this.onAnimationFrame);
+		var _onAnimationFrame = function(){
+			viewY += (scrollToY - viewY) * opt.friction;
+			window.scrollTo(scrollToX, Math.round(viewY));
+			if(window.pageYOffset !== scrollToY){
+				raf = requestAnimationFrame(_onAnimationFrame);
 			}else{
-				this.scrollwheel = false;
+				scrollwheel = false;
 			}
-		}
-	});
+		};
 
-	$.plugin('utils.smoothwheel', smoothwheel);
-	return smoothwheel;
-});
+		$window.on('resize', function () {
+			viewHeight = $window.height();
+		}).on('scroll', function () {
+			scrollToX = $body.scrollLeft();
+			if (!scrollwheel) scrollToY = viewY = $body.scrollTop();
+		});
 
+		$body.mousewheel(function(event){
+			var deltaFactor = event.deltaFactor * opt.deltaSteps/Math.abs(event.deltaFactor);
+			scrollwheel = true;
+			if(event.deltaY < 0){
+				scrollToY = ((scrollToY + viewHeight) < $document.height()) ? scrollToY + deltaFactor : $body.scrollTop();
+			}else{
+				scrollToY = 0 < scrollToY ? scrollToY - deltaFactor : 0;
+			}
+			raf = requestAnimationFrame(_onAnimationFrame);
+			return false;
+		});
+	};
+}));
